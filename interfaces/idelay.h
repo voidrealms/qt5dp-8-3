1c54225d0b6837625219f3fa118296677690c4ef
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:37:23 -0400

    first commit

---
+++ interfaces/idelay.h
@@ -0,0 +1,16 @@
+#ifndef IDELAY_H
+#define IDELAY_H
+
+#include <QObject>
+
+class iDelay
+{
+
+public:
+
+    virtual void delay(int ms) = 0;
+    virtual bool isComplete() = 0;
+    virtual ~iDelay() {} //This will annoy people
+};
+
+#endif // IDELAY_H
